12cb833516bb8d8d63832e7bd95f74f4ab4dfc78
borosboyo <borosgergo00@gmail.com>
2023-03-13 15:43:04 +0100

    init2

---
+++ collectoro-backend/src/main/kotlin/hu/bme/aut/collectoro/CollectoroBackendApplication.kt
@@ -0,0 +1,11 @@
+package hu.bme.aut.collectoro
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class CollectoroBackendApplication
+
+fun main(args: Array<String>) {
+    runApplication<CollectoroBackendApplication>(*args)
+}
---
+++ collectoro-backend/src/test/kotlin/hu/bme/aut/collectoro/CollectoroBackendApplicationTests.kt
@@ -0,0 +1,13 @@
+package hu.bme.aut.collectoro
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class CollectoroBackendApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
