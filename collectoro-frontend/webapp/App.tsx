0332f0b083ebad4bdd9822c32673a2d9d99face4
borosboyo <borosgergo00@gmail.com>
2023-03-15 22:25:54 +0100

    Make axios work

--- collectoro-frontend/webapp/App.tsx
+++ collectoro-frontend/webapp/App.tsx
@@ -1,28 +1,44 @@
 import { StatusBar } from 'expo-status-bar';
 import {Button, StyleSheet, Text, View} from 'react-native';
 import React, { useState, useEffect } from 'react'
-import {GetUserByIdTsReq} from "./swagger";
+import {Configuration, GetUserByIdTsReq} from "./swagger";
+import {UserControllerApi} from "./swagger";
+import {UserControllerApiFactory as userController} from "./swagger";
+import AsyncStorage from "@react-native-async-storage/async-storage";
+import globalAxios, {AxiosInstance, AxiosRequestConfig} from "axios";
+import axios from "axios";
 
+const baseOptions : AxiosRequestConfig = {
+    baseURL: "http://localhost:8080",
+    headers: {
+        'Content-Type': 'application/json', 'Accept': 'application/json', 'Authorization': 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJib3Jvc2dlcmdvMDBAZ21haWwuY29tIiwiaWF0IjoxNjc4OTE1NDgyLCJleHAiOjE2Nzg5MTY5MjJ9.QgQxyc8uPAf77uhANRJr192QIyOZ2Pn27O2mXpiuEy0'
+    }
+}
 
+const config : Configuration = new Configuration({
+    baseOptions: baseOptions
+})
 
 export default function App() {
   const [token, setToken] = useState<string>(null);
 
   useEffect(() => {
+    AsyncStorage.setItem('token', '1234567890')
+
+
   }, []);
 
 
   const getUser = async () => {
-  }
+    const req : GetUserByIdTsReq = {
+        id: 1
+    }
 
-  const getDemo = async () => {
-    fetch(`http://10.0.2.2:8080/api/demo`, {
-      method: 'GET', headers: {
-        'Content-Type': 'application/json', 'Accept': 'application/json'
-      },
+    console.log(config.baseOptions);
+
+    userController(config).getUserById(req).then((res) => {
+        console.log(res);
     })
-        .then((response) => response.json())
-        .then(data => console.log(data));
   }
 
   return (
